ed49ec43052159d2acbc92b0eb1638bfba1909c0
Ali Bulut <alibulut@yahoo.com>
2020-03-09 20:14:24 +0300

    deploying

--- helpers/redisStore.js
+++ helpers/redisStore.js
@@ -7,12 +7,12 @@
 const redis=require('redis');
 const session=require('express-session');
 const redisStore=require('connect-redis')(session);
-const client=redis.createClient();
+const client=redis.createClient({
+    port:process.env.REDIS_PORT,
+    host:process.env.REDIS_URI
+});
 
 
 module.exports=new redisStore({
-    client,
-    host: process.env.REDIS_URI,
-    port: process.env.REDIS_PORT,
-    pass:process.env.REDIS_PASS
+    client
 })
--- src/redisClient.js
+++ src/redisClient.js
@@ -2,9 +2,9 @@ const redis=require('redis');
 
 const getClient=()=>{
     return redis.createClient({
-        host:process.env.REDIS_URI,
         port:process.env.REDIS_PORT,
-        password:process.env.REDIS_PASS
+        host:process.env.REDIS_URI,
+        auth_pass:process.env.REDIS_PASS
     })
 }
 
